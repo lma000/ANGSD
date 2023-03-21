d43e5fe7f3cc0917bd7475982276d714874525a0
Linda Ma <66576779+lma000@users.noreply.github.com>
2023-03-21 15:30:20 -0400

    Create align.sh

---
+++ align.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+
+#SBATCH --partition=angsd_class
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=align_reads
+#SBATCH --output=../slurm/align.out
+#SBATCH --time=24:00:00
+#SBATCH --mem=40G
+#SBATCH --mail-user=lim4019@med.cornell.edu
+#SBATCH --mail-type=ALL
+#SBATCH --cpus-per-task=10
+
+mamba activate angsd
+
+for folder in $(cat conditions); do 
+mkdir -p alignments/$folder
+for file1 in $folder/*_1.fastq.gz; do
+file2=${file1/_1/_2}
+    
+STAR --runMode alignReads --runThreadN 10 --genomeDir /athena/angsd/scratch/lim4019/project2/index/index --readFilesIn $file1 $file2 --readFilesCommand zcat --outFileNamePrefix alignments/${file1%%_1.fastq.gz}. --outSAMtype BAM SortedByCoordinate --outSAMattributes MD
+
+done
+done
