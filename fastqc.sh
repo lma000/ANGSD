d3099179a611380a3ae402efc7a81691d226ed1c
Linda Ma <66576779+lma000@users.noreply.github.com>
2023-03-28 17:07:31 -0400

    Create fastqc.sh

---
+++ fastqc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#SBATCH --partition=angsd_class
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=fastqc
+#SBATCH --output=../slurm/fastqc.out
+#SBATCH --time=12:00:00
+#SBATCH --mem=40G
+#SBATCH --mail-user=lim4019@med.cornell.edu
+#SBATCH --mail-type=ALL
+#SBATCH --cpus-per-task=1
+
+mamba activate angsd
+
+fastqc -o fastqc */*fastq.gz
